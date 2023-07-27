a4e5871e0762f03795d036bdeed5038eadfaca3d
lucas moltedo <lucasmoltedo03@gmail.com>
2023-07-27 01:16:02 -0300

    Avance entidades del modelo de dominio

--- src/main/java/ar/edu/utn/frbb/tup/model/Alumno.java
+++ src/main/java/ar/edu/utn/frbb/tup/model/Alumno.java
@@ -1,5 +1,96 @@
 package ar.edu.utn.frbb.tup.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Alumno {
+
+    // Atributos --------------------------------------------------------------
+
+    private long id;
+
+    private String nombre;
+    private String apellido;
+    private long dni;
+
+    private List<Asignatura> asignaturas;
+
+    // ------------------------------------------------------------------------
+
+    // Constructores ----------------------------------------------------------
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String apellido, long dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+
+        asignaturas = new ArrayList<>();
+    }
+
+    // ------------------------------------------------------------------------
+
+    // Getters y Setters ------------------------------------------------------
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    // ------------------------------------------------------------------------
+
+    // Métodos relacionados con asignaturas -----------------------------------
+
+    public void agregarAsignatura(Asignatura asignatura){
+        this.asignaturas.add(asignatura);
+    }
+
+    public List<Asignatura> obtenerListaAsignaturas(){
+        return this.asignaturas;
+    }
+
+    public void aprobarAsignatura() {
+    }
+
+    private void chequearCorrelatividad() {
+    }
+
+    public boolean puedeAprobar(Asignatura asignatura) {
+        return true;
+    }
+
+    public void actualizarAsignatura() {
+    }
+
+    // ------------------------------------------------------------------------
 
 }
--- src/main/java/ar/edu/utn/frbb/tup/model/Materia.java
+++ src/main/java/ar/edu/utn/frbb/tup/model/Materia.java
@@ -1,4 +1,113 @@
 package ar.edu.utn.frbb.tup.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Materia {
+
+    // Atributos --------------------------------------------------------------
+
+    private int materiaId;
+    private String nombre;
+    private int anio;
+    private int cuatrimestre;
+    private Profesor profesor;
+
+    private List<Materia> correlatividades;
+
+    // ------------------------------------------------------------------------
+
+    // Constructores ----------------------------------------------------------
+
+    public Materia(){}
+
+    public Materia(String nombre, int anio, int cuatrimestre, Profesor profesor) {
+        this.anio = anio;
+        this.cuatrimestre = cuatrimestre;
+        this.nombre = nombre;
+        this.profesor = profesor;
+
+        correlatividades = new ArrayList<>();
+    }
+
+    // ------------------------------------------------------------------------
+
+    // Getters y Setters ------------------------------------------------------
+
+    public int getMateriaId() {
+        return materiaId;
+    }
+
+    public void setMateriaId(int materiaId) {
+        this.materiaId = materiaId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getCuatrimestre() {
+        return cuatrimestre;
+    }
+
+    public void setCuatrimestre(int cuatrimestre) {
+        this.cuatrimestre = cuatrimestre;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public List<Materia> getCorrelatividades() {
+        return correlatividades;
+    }
+
+    public void setCorrelatividades(List<Materia> correlatividades) {
+        this.correlatividades = correlatividades;
+    }
+
+    // ------------------------------------------------------------------------
+
+    // Métodos relacionados con correlatividades  -----------------------------
+
+    public void agregarCorrelatividad(Materia materia){
+        this.correlatividades.add(materia);
+    }
+
+    // ------------------------------------------------------------------------
+
+    // Equals y HashCode ------------------------------------------------------
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Materia materia = (Materia) o;
+        return materiaId == materia.materiaId && anio == materia.anio && cuatrimestre == materia.cuatrimestre && Objects.equals(nombre, materia.nombre) && Objects.equals(profesor, materia.profesor) && Objects.equals(correlatividades, materia.correlatividades);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(materiaId, nombre, anio, cuatrimestre, profesor, correlatividades);
+    }
+
+    // ------------------------------------------------------------------------
+
 }
